d2de9d29a5cbeeef0c672b1c79cb4af09d858f91
Erik Menchaca <Erik@mail.rit.edu>
2023-11-17 15:12:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+    $page = "index";
+include("assets/inc/index-header.php");
+include("assets/inc/index-nav.php");
+?>
+<main>
+    <p>Welcome to my project site. Here you will find some quick information of visiting and living on RIT campus as
+        a full-time student.</p>
+    <h2>Benefits of being NTID/RIT student</h2>
+    <ul>
+        <li>Hand-on learning experience </li>
+        <li>Provided with DSO accommodations</li>
+        <li>NTID Access Services (ASL interpreting, notetaking, captioning)</li>
+        <li>Getting a Co-op (NC3, help with resumes and cover letters)</li>
+    </ul>
+    <h2>Academic Programs Offered</h2>
+    <h3>Department of American Sign Language and Interpreting Program</h3>
+    <ul>
+        <li>ASL-English Interpretation BS</li>
+        <li>Health Care Interpretation MS</li>
+        <li>Certificate in Educational Interpreting</li>
+        <li>Certificate in Health Care Interpreting</li>
+        <li>Certificate in Deaf Interpreting</li>
+        <li>Certificate in Trilingual Interpreting</li>
+        <li>Certificate in Signed Language Translation</li>
+    </ul>
+    <h3>Department of Business Studies</h3>
+    <ul>
+        <li>Accounting Technology AAS</li>
+        <li>Administrative Support Technology</li>
+        <li>Business Administration AAS</li>
+        <li>Certificate in Health Care Interpreting</li>
+        <li>Business AS</li>
+        <li>Business Technology AOS</li>
+    </ul>
+    <h3>Department of Engineering Studies</h3>
+    <ul>
+        <li>Applied Mechanical Technology AAS</li>
+        <li>Architectural and Civil Drafting Technology AAS</li>
+        <li>Architectural and Civil Drafting Technology AOS</li>
+        <li>Civil Technology AAS</li>
+        <li>Pre-Baccalaureate Studies in Engineering</li>
+        <li>Precision Manufacturing Technology AOS</li>
+    </ul>
+    <h3>Department of Information and Computing Studies</h3>
+    <ul>
+        <li>Applied Computer Technology AAS</li>
+        <li>Applied Computer Technology AOS</li>
+        <li>Applied Computer Technology AS</li>
+        <li>Mobile Application Development AAS</li>
+    </ul>
+    <h3>Department of Liberal Studies</h3>
+    <ul>
+        <li>Applied Liberal Arts AS</li>
+        <li>Community Development and Inclusive Leadership</li>
+        <li>Deaf Cultural Studies-American Sign Language Certificate</li>
+        <li>Pre-Baccalaureate Studies in Liberal Arts</li>
+        <li>American Sign Language and Deaf Cultural Studies Minor and Immersion</li>
+        <li>Deaf Cultural Studies/ASL Certificate</li>
+        <li>Deaf Leadership Minor and Immersion</li>
+        <li>Diversity, Inclusion, and Dialogue Minor and Immersion</li>
+    </ul>
+    <h3>Department of Master of Science in Secondary Education</h3>
+    <ul>
+        <li>Secondary Education of Students Who Are Deaf or Hard of Hearing</li>
+    </ul>
+    <h3>Departing of Performing Arts</h3>
+    <ul>
+        <li>Minor and Immersion in Theatre Design and Stagecraft</li>
+        <li>Minor and Immersion in Dance</li>
+        <li>Minor in ASL Performance</li>
+    </ul>
+    <h3>Department of Science and Math</h3>
+    <ul>
+        <li>General Science AS</li>
+        <li>Laboratory Science Technology AAS</li>
+        <li>Laboratory Science Technology AOS</li>
+        <li>Pre-Baccalaureate Studies in Science and Mathematics</li>
+    </ul>
+    <h3>Department of Visual Communication Studies</h3>
+    <ul>
+        <li>3D Graphics Technology</li>
+        <li>Design and Imaging Technology AAS</li>
+        <li>Design and Imaging Technology AOS</li>
+        <li>Pre-Baccalaureate Studies in Visual Communications</li>
+    </ul>
+</main>
+<?php
+include("assets/inc/index-footer.php");
+?>
